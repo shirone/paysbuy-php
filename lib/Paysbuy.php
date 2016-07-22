77f51c38b91758f5d37963ecec60fa59ae17b200
Jon Randy <jonathan@paysbuy.com>
2016-07-22 12:12:47 +0700

    Added defaults for URLs and test mode so user can omit them

--- lib/Paysbuy.php
+++ lib/Paysbuy.php
@@ -1,5 +1,9 @@
 <?php
 
+defined('PAYSBUY_LIVE_DOMAIN') || define('PAYSBUY_LIVE_DOMAIN', 'https://www.paysbuy.com');
+defined('PAYSBUY_TEST_DOMAIN') || define('PAYSBUY_TEST_DOMAIN', 'https://demo.paysbuy.com');
+defined('PAYSBUY_TESTMODE') || define('PAYSBUY_TESTMODE', FALSE);
+
 $main = dirname(__FILE__)."/paysbuy/";
 
 require_once $main.'PaysbuyPaynow.php';
